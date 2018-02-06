449385c8d6f2e251fa59f52aa88b13820c2d8f9b
Ziwei Huang <z.huangcsee@gmail.com>
2018-02-06 23:02:27 +0100

    add solution 1

--- two_sum.java
+++ two_sum.java
@@ -11,6 +11,28 @@ Because nums[0] + nums[1] = 2 + 7 = 9,
 return [0, 1].
 */
 
+/*
+Solution 1 O(n2)
+*/
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        for(int i=0; i<nums.length; i++){
+            for(int j=i+1; j<nums.length; j++){
+                if(nums[i] == target - nums[j]){
+                    result[0] = i;
+                    result[1] = j;
+                }
+            }
+        }
+        return result;
+    }
+}
+
+
+/*
+Solution2 O(n)
+*/
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         int[] result = new int[2];
