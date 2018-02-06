e5ea3cf3f9d1e46e2f6fbfb95328a4508a91cc1c
Ziwei Huang <z.huangcsee@gmail.com>
2018-02-06 23:34:34 +0100

    add solution 3 with comment

--- two_sum.java
+++ two_sum.java
@@ -31,7 +31,7 @@ class Solution {
 
 
 /*
-Solution2 O(n)
+Solution 2 O(n)
 */
 class Solution {
     public int[] twoSum(int[] nums, int target) {
@@ -46,5 +46,27 @@ class Solution {
             }
         }
         return result;        
+    }
+}
+
+/*
+Solution 3
+*/
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        HashMap<Integer,Integer> map = new HashMap();
+        for(int i=0; i<nums.length; i++){
+            map.put(nums[i],i);
+        }
+        for(int i=0; i<nums.length; i++){
+        	// remember to check map.get(target-nums[i])!=i
+            if(map.containsKey(target-nums[i]) && map.get(target-nums[i])!=i){
+                result[0] = i;
+                result[1] = map.get(target-nums[i]);
+                break; // remember to break
+            }          
+        }
+        return result;
     }
 }
